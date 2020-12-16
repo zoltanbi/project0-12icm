ecc97a79b351056f37b4e04bb0e4a4f4d1f5269c
Zoltanbi <zoltanbiro001@gmail.com>
2020-12-15 21:52:08 -0800

    Added shop plugin code into file. Created preliminary ranking system for the gamemode. Contains classes FileManager, InfectedRank, InfectedRankCommand, Main, Prestige, Rank, RankCommand, RankListener and SetPrestigeCommand.

---
+++ Rank/src/com/infected/ranks/Prestige.java
@@ -0,0 +1,35 @@
+package com.infected.ranks;
+
+public enum Prestige {
+
+    ONE("I-", "❄"),
+    TWO("II-", "❊"),
+    THREE("III-", "✽"),
+    FOUR("IV-", "❋"),
+    FIVE("V-", "❇"),
+    SIX("VI-", "✵"),
+    SEVEN("VII-", "❀"),
+    EIGHT("VIII-", "❁"),
+    NINE("IX-", "✬"),
+    TEN("X-", "✫"),
+    ELEVEN("XI-", "✷"),
+    TWELVE("XII-", "✺"),
+    THIRTEEN("XIII-", "✪"),
+    FOURTEEN("XIV-", "❂"),;
+
+    private String prNum;
+    private String prSymbol;
+
+    Prestige(String prNum, String prSymbol) {
+        this.prNum = prNum;
+        this.prSymbol = prSymbol;
+    }
+
+    public String getPrNum() {
+        return prNum;
+    }
+
+    public String getPrSymbol() {
+        return prSymbol;
+    }
+}
